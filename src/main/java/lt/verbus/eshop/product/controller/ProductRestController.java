bd18962bde0b13174830e5d4f42869606f336b81
Sarunas Verbus <sarunas@verbus.lt>
2020-12-29 22:10:59 +0200

    add separate REST controller

--- src/main/java/lt/verbus/eshop/product/controller/ProductRestController.java
+++ src/main/java/lt/verbus/eshop/product/controller/ProductRestController.java
@@ -5,13 +5,14 @@ import lt.verbus.eshop.product.service.ProductService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/public/product")
+@RequestMapping("/public/api/product")
 public class ProductRestController {
 
     private final ProductService productService;
@@ -20,14 +21,20 @@ public class ProductRestController {
         this.productService = productService;
     }
 
-    @GetMapping(value = "/rest/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> getAllProductsAsJson(){
         return productService.getPageOfProducts(PageRequest.of(0, 10)).getContent();
     }
 
-    @GetMapping("/rest/xml")
+    @GetMapping("/xml")
     public List<Product> getAllProductsAsXml(){
         return productService.getPageOfProducts(PageRequest.of(0, 10)).getContent();
     }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product getProduct(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
 
 }
